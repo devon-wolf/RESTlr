fda1d6819bb0fe3a6ab0f5d8ffa5e35bc0d25762
Devon Wolfkiel <devonwolf@tutanota.com>
2021-05-13 15:46:29 -0700

    Get controls fully wired up to state

--- src/components/controls/SearchForm.jsx
+++ src/components/controls/SearchForm.jsx
@@ -2,15 +2,92 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import style from './SearchForm.css';
 
-const SearchForm = () => (
-	<form className={style.searchForm}>
-		<input />
+const SearchForm = ({ handleFormSubmit, handleAddressChange, handleRadioChange, handleBodyChange, addressValue, radioValue, bodyValue }) => (
+	<form
+		className={style.searchForm}
+		onSubmit={handleFormSubmit}
+		aria-label="request-form"
+	>
+		<input
+			value={addressValue}
+			aria-label="address field"
+			onInput={handleAddressChange}
+		/>
+
+		<label>
+			GET
+			<input
+				type="radio"
+				name="method"
+				value="GET"
+				checked={radioValue === 'GET'}
+				onChange={handleRadioChange}
+			/>
+		</label>
+
+		<label>
+			POST
+			<input
+				type="radio"
+				name="method"
+				value="POST"
+				checked={radioValue === 'POST'}
+				onChange={handleRadioChange}
+			/>
+		</label>
+
+		<label>
+			PUT
+			<input
+				type="radio"
+				name="method"
+				value="PUT"
+				checked={radioValue === 'PUT'}
+				onChange={handleRadioChange}
+			/>
+		</label>
+
+		<label>
+			PATCH
+			<input 
+				type="radio"
+				name="method"
+				value="PATCH"
+				checked={radioValue === 'PATCH'}
+				onChange={handleRadioChange}
+			/>
+		</label>
+
+		<label>
+			DELETE
+			<input
+				type="radio"
+				name="method"
+				value="DELETE"
+				checked={radioValue === 'DELETE'}
+				onChange={handleRadioChange}
+			/>
+		</label>
+
+		<textarea 
+			value={bodyValue}
+			aria-label="request-body"
+			onInput={handleBodyChange}
+		>
+		</textarea>
+
 		<button>Send Request</button>
 	</form>
 );
 
 SearchForm.propTypes = {
-
+	handleFormSubmit: PropTypes.func.isRequired,
+	handleAddressChange: PropTypes.func.isRequired,
+	handleRadioChange: PropTypes.func.isRequired,
+	handleBodyChange: PropTypes.func.isRequired,
+	addressValue: PropTypes.string.isRequired,
+	radioValue: PropTypes.string.isRequired,
+	bodyValue: PropTypes.string.isRequired
 };
 
 export default SearchForm;
--- src/containers/MainPage.jsx
+++ src/containers/MainPage.jsx
@@ -5,11 +5,45 @@ import Results from '../components/displays/Results';
 import HistoryList from '../components/displays/HistoryList';
 
 export default class MainPage extends Component {
+	state = {
+		address: 'endpoint goes here',
+		radio: 'GET',
+		body: 'JSON goes here'
+	}
+
+	handleFormSubmit = async e => {
+		e.preventDefault();
+		console.log('FORM SUBMITTED');
+	}
+
+	handleAddressChange = e => {
+		this.setState({ address: e.target.value });
+	}
+
+	handleRadioChange = e => {
+		this.setState({ radio: e.target.value });
+
+	}
+
+	handleBodyChange = e => {
+		this.setState({ body: e.target.value });
+	}
+
+
 	render() {
+		const { address, radio, body } = this.state;
 		return (
 			<div className={style.mainPage}>
 				<header>RESTLAB</header>
-				<SearchForm />
+				<SearchForm 
+					handleFormSubmit={this.handleFormSubmit}
+					handleAddressChange={this.handleAddressChange}
+					handleRadioChange={this.handleRadioChange}
+					handleBodyChange={this.handleBodyChange}
+					addressValue={address}
+					radioValue={radio}
+					bodyValue={body}
+				/>
 				<Results 
 					results="this is a result"
 				/>
